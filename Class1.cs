fa9845e1dd0dcf4e10c168afdf6c29540523980f
kazai_mazai <kazaimazai@gmail.com>
2013-09-01 16:01:04 +0400

    first

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WealthLab.DataProviders.Common;
+
+namespace WealthLabDataSource
+{
+    public class Class1
+    {
+        public  void Wow()
+        {
+            var r = new RoadRunner();
+          var bars =  r.RequestHistoricalData("AAPL");
+            
+
+
+        }
+
+    }
+}
